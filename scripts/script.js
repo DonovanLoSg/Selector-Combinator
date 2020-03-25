887be5af187c82a59d484f56c1458b03a88ae47c
DonovanLoSg <donovanlo1174@gmail.com>
2020-03-25 19:01:53 +0000

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function resetCSS(){
+    var selectedSelector = document.querySelectorAll('#targetted-box p');
+    var i=0;
+    for (i = 0; i < selectedSelector.length; ++i) {
+        selectedSelector[i].style.color = "black";
+        selectedSelector[i].style.fontWeight = "normal";}
+}
+
+
+function changeCSS(item){
+    resetCSS();
+    var selectedSelector = document.querySelectorAll('#targetted-box '+item);
+    var i=0;
+    for (i = 0; i < selectedSelector.length; ++i) {
+        selectedSelector[i].style.color = "red";
+        selectedSelector[i].style.fontWeight = "bold";}
+}
+
+
